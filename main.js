624e40769a7808685035eca941c9e8a55dcacd56
55973 <55973@etu.he2b.be>
2023-01-17 09:28:16 +0100

    exo 1

---
+++ main.js
@@ -0,0 +1,16 @@
+// # Exercice :
+// >*En utilisant l'attribut style*
+
+// >*Cherche des methodes pour résoudre les énoncés suivants :*
+
+
+// ### 1. Récupère le h1 dans une variable 
+let exo1 = document.querySelector('h1')
+console.log(exo1);
+
+// ### 2. Créer un programme qui met le texte du h1 en bleu
+
+// ### 3. Met ce programme dans une function
+// ### 4. Appelle cette function pour qu'elle soit executé 
+// ### 5. Met un écouteur d'événement sur le h1, qui au clique lance la function
+
